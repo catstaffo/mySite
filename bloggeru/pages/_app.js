aeb81ae6649e6c0309cf0b5d2a881351be060d7c
Catherine Stafford <catstaffo@gmail.com>
2023-01-03 23:35:50 -0500

    tailwind added and preliminary layout

--- bloggeru/pages/_app.js
+++ bloggeru/pages/_app.js
@@ -1,10 +1,8 @@
 import '../styles/globals.css'
-import Nav from '../components/Nav'
 
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <Nav />
       <Component {...pageProps} />
     </>
   );
