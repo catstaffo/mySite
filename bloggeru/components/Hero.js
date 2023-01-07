d3d5e30a505739b6384b6b54b92af0ac414e264b
Catherine Stafford <catstaffo@gmail.com>
2023-01-07 14:22:48 -0500

    more content : NEED TO CLEAN UP CODE

--- bloggeru/components/Hero.js
+++ bloggeru/components/Hero.js
@@ -2,7 +2,6 @@ import Image from 'next/image';
 import Link from 'next/link';
 
 export default function Hero(){
-    // 01110111 01100101 01101100 01100011 01101111 01101101 01100101 00100001 00100000 01110100 01101000 01101001 01110011 00100000 01110011 01101001 01110100 01100101 00100000 01101001 01110011 00100000 01100001 00100000 01110000 01100101 01110010 01110011 01101111 01101110 01100001 01101100 00100000 01110000 01110010 01101111 01101010 01100101 01100011 01110100 00101101 00101101 00100000 01100110 01100101 01100101 01101100 00100000 01100110 01110010 01100101 01100101 00100000 01110100 01101111 00100000 01100101 01111000 01110000 01101100 01101111 01110010 01100101 00100001: welcome! this site is a personal project-- feel free to explore!
     return (
         <section className="py-12">
             <div className="container mx-auto md:px-20">
@@ -22,10 +21,21 @@ function Divider(){
                 </div>
             </div>
             <div className="right-info box">
-                <div className="small-text px-5">
-                    <p className="font-light text-sm">my collection of <Link href="/" className="intext">tools</Link>, <Link href="/" className="intext">thoughts</Link>, and <Link href="/" className="intext">work</Link></p>
-                </div>
+                <div className="px-5">
+                    <div className="small-text">
+                        <p className="font-light text-sm">my collection of <Link href="/" className="intext">tools</Link>, <Link href="/" className="intext">thoughts</Link>, and <Link href="/" className="intext">work</Link></p>
+                        <div className="parallelogram float-right z-0 flex"></div>
+                    </div>
+                    <div className="welcome py-5">
+                        <p className="welcome-text">welcome!<br></br> this site is a personal project-- feel free to explore!</p>
+                        <div className="parallelogram float-right z-0 flex"></div>
+                        <p className="binary-text pt-3">
+                            01110111 01100101 01101100 01100011 01101111 01101101 01100101 00100001 00100000 01110100 01101000 01101001 01110011 00100000 01110011 01101001 01110100 01100101 00100000 01101001 01110011 00100000 01100001 00100000 01110000 01100101 01110010 01110011 01101111 01101110 01100001 01101100 00100000 01110000 01110010 01101111 01101010 01100101 01100011 01110100 00101101 00101101 00100000 01100110 01100101 01100101 01101100 00100000 01100110 01110010 01100101 01100101 00100000 01110100 01101111 00100000 01100101 01111000 01110000 01101100 01101111 01110010 01100101 00100001</p>
+                        <div className="parallelogram float-right z-0 flex"></div>
+                    </div>
+                <img src={"/images/xorgate.png"} alt="state machine" className="max-h-[60px] z-20 float-right overflow-visible"/>
             </div>
+        </div>
         </div>
     )
 }
